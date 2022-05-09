8b8931af287cad185887ecac85c0885bc67de458
panguojun <panguojun@users.noreply.github.com>
2022-05-09 22:10:07 +0800

    Update SCEPHG.hpp

--- SCEPHG.hpp
+++ SCEPHG.hpp
@@ -97,174 +97,7 @@ namespace ScePHG
 		POP_SPARAM;
 		return 0;
 	}
-
-	inline string fixedname(crstr name)
-	{
-		const char* p = name.c_str();
-		if (*p == '\'' || *p == '\"')
-		{
-			string ret = p + 1;
-			ret.pop_back();
-			return ret;
-		}
-		return name;
-	}
-	inline void fixedproperty(string& str)
-	{
-		str.erase(std::remove(str.begin(), str.end(), '\''), str.end());
-		str.erase(std::remove(str.begin(), str.end(), '\"'), str.end());
-
-	}
-	inline rect_t torect(crstr str)
-	{
-		rect_t rect;
-		sscanf_s(str.c_str(), "{x:%d,y:%d,width:%d,height:%d}",  &rect.x, &rect.y, &rect.width, &rect.height);
-		return rect;
-	}
-	API(getrect)
-	{
-		rectlist.clear();
-		NODE* node = ROOT;
-		if (args > 0)
-		{
-			string param1 = GET_SPARAM(1);
-			node = GET_NODE(param1, ROOT);
-			if (!node)
-				return 0;
-		}
-		string key = "rect";
-		if (args > 1)
-		{
-			key = GET_SPARAM(2);
-		}
-		node_walker(node, [key](tree_t* tree)->void
-			{
-				auto& it = tree->kv.find(key);
-				if (it != tree->kv.end())
-				{
-					string str= it->second;
-					fixedproperty(str);
-					rectlist.push_back(torect(str));
-				}
-			});
-		POP_SPARAM;
-		return 0;
-	}
-	inline vec3 tovec3(crstr str)
-	{
-		vec3 v;
-		sscanf_s(str.c_str(), "%f,%f,%f", &v.x, &v.y, &v.z);
-		return v;
-	}
-	API(getvec3)
-	{
-		vec3list.clear();
-		NODE* node = ROOT;
-		if (args > 0)
-		{
-			string param1 = GET_SPARAM(1);
-			node = GET_NODE(param1, ROOT);
-			if (!node)
-				return 0;
-		}
-		string key = "pos";
-		if (args > 1)
-		{
-			key = GET_SPARAM(2);
-		}
-		node_walker(node, [key](tree_t* tree)->void
-			{
-				auto& it = tree->kv.find(key);
-				if (it != tree->kv.end())
-				{
-					string str = it->second;
-					fixedproperty(str);
-					vec3list.push_back(tovec3(str));
-				}
-			});
-		POP_SPARAM;
-		return 0;
-	}
-	API(getfval)
-	{
-		reallist.clear();
-		NODE* node = ROOT;
-		if (args > 0)
-		{
-			string param1 = GET_SPARAM(1);
-			node = GET_NODE(param1, ROOT);
-			if (!node)
-				return 0;
-		}
-		string key = "x";
-		if (args > 1)
-		{
-			key = GET_SPARAM(2);
-		}
-		node_walker(node, [key](tree_t* tree)->void
-			{
-				auto& it = tree->kv.find(key);
-				if (it != tree->kv.end())
-				{
-					string str = it->second;
-					fixedproperty(str);
-					reallist.push_back(atof(str.c_str()));
-				}
-			});
-		POP_SPARAM;
-		return 0;
-	}
-	API(getival)
-	{
-		intlist.clear();
-		NODE* node = ROOT;
-		if (args > 0)
-		{
-			string param1 = GET_SPARAM(1);
-			node = GET_NODE(param1, ROOT);
-			if (!node)
-				return 0;
-		}
-		string key = "x";
-		if (args > 1)
-		{
-			key = GET_SPARAM(2);
-		}
-		node_walker(node, [key](tree_t* tree)->void
-			{
-				auto& it = tree->kv.find(key);
-				if (it != tree->kv.end())
-				{
-					string str = it->second;
-					fixedproperty(str);
-					intlist.push_back(atoi(str.c_str()));
-				}
-			});
-		POP_SPARAM;
-		return 0;
-	}
 	
-	API(getstr)
-	{
-		string param1 = GET_SPARAM(1);
-		string param2 = GET_SPARAM(2);
-		
-		//strlist.clear();
-		ScePHG::node_walker(ROOT, [param1, param2](ScePHG::tree_t* tree)->void
-			{
-				if (tree->name == param1) {
-					auto& it = tree->kv.find(param2);
-					if (it != tree->kv.end())
-					{
-						strlist.push_back(it->second.c_str());
-					}
-				}
-			});
-		POP_SPARAM;
-
-		PRINTV(strlist.size());
-		return 0;
-	}
 #ifdef XML
 // ----------------------------------------
 #include "xmlparser.hpp"
@@ -298,22 +131,7 @@ namespace ScePHG
 			JSON_PARSER::tojson_raw(ROOT);
 		return 0;
 	}
-#ifdef PYTHON
-	API(formImgIDF)
-	{
-		SPARAM(img);
-
-		ASSERT(ME);
-
-		string json;
-		python_interface::detect_rectangle(json, img);
-
-		JSON_PARSER::fromJSON(ME,json);
-
-		return 0;
-	}
-#endif
-
+	
 	// phgoper
 	void phgoper(tree_t* tree)
 	{
@@ -354,12 +172,7 @@ namespace ScePHG
 
 		return 0;
 	}
-
-	// constraint
-	/*API(constraint)
-	{
-		return 0;
-	}*/
+	
 	// -----------------------------------
 	// REG API
 	// -----------------------------------
@@ -378,26 +191,6 @@ namespace ScePHG
 
 		REG_API(setup, setuptree);		// 生成节点树
 		
-		// NODE: 
-
-		REG_API(cur, cur);				// ME
-		REG_API(array, array);			// 节点阵列
-		REG_API(sequ, sequ);			// 节点序列
-
-		REG_API(addprop, addprop);		// 添加属性
-		
-		REG_API(getival, getival);		// 获得int value
-		REG_API(getfval, getfval);		// 获得float value
-		REG_API(getstr, getstr);		// 获得string
-		REG_API(getvec3, getvec3);		// 获得RECT
-		REG_API(getrect, getrect);		// 获得RECT
-
-#ifdef XML
-		REG_API(fromXML, fromXML);		// xml读入
-#endif
-#ifdef PYTHON
-		REG_API(formImgIDF, formImgIDF);// 图像识别读入
-#endif
 		REG_API(tojson, tojson);
 
 		REG_API(tojsonraw, tojsonraw);
@@ -409,12 +202,10 @@ namespace ScePHG
 		REG_API(phg, phgoper);			// 脚本
 
 		REG_API(draw, draw);			// 绘制
-
-		// CONSTRAINT
-
-		//REG_API(constraint, constraint);// 约束
-
-		SPRITE_REG_API();				// sprite 注册
+		
+		NODE_REG_API();
+		
+		SPRITE_REG_API();			// sprite 注册
 	}
 };
 // ====================================
