150a197302d910e5425a2f575bde0034ed61d39b
panguojun <panguojun@users.noreply.github.com>
2022-05-16 13:44:00 +0800

    Update element.hpp

--- base/element.hpp
+++ base/element.hpp
@@ -1,6 +1,6 @@
 /**************************************************************************
-*							元素
-*						  用于运算
+*							鍏冪礌
+*						  鐢ㄤ簬杩愮畻
 **************************************************************************/
 #ifdef ELEMENT
 #undef ELEMENT
@@ -21,11 +21,19 @@ struct var_t : public varbase_t
 {
 	VAR_BASE(var_t);
 	var_t() { }
-
-	// 资源加法函数
+	var_t(const varbase_t& v)
+	{
+		//PRINT("varbase_t copy " << v.type);
+		(*(varbase_t*)this) = v;
+	}
+	// 璧勬簮鍔犳硶鍑芥暟
 	std::function<var_t(var_t& a,var_t& b)> fun_add = 0;
 
-	var_t operator + (var_t& v)
+	inline void operator = (const var_t& v)
+	{
+		varbase_t::operator=(v);
+	}
+	inline var_t operator + (var_t& v)
 	{
 		//PRINT("operator + " << v.resid << " " << resid)
 
@@ -37,87 +45,101 @@ struct var_t : public varbase_t
 			return fun_add(*this, v);
 		}
 	}
+	inline var_t operator - () const
+	{
+		//PRINT("-")
+		return varbase_t::operator-();
+	}
 };
 
 inline void _PHGPRINT(const std::string& pre, const var& v)
 {
-	//if (v.sval == "")
-	PRINT(pre << v.ival)
-		//else
-		//	PRINT(pre << v.sval)
+	PRINTV(v.type)
+	if(v.type == 1)
+		PRINT(pre << v.ival)
+	else if (v.type == 2)
+		PRINT(pre << v.fval)
+	else
+		PRINT(pre << "unkown type")
 }
 
 // ------------------------------------------
 #include "phg.hpp"
 // ------------------------------------------
-// 运算
+// 杩愮畻
+using fun_calc_t = std::function<string(code& cd, char o, int args)>;
+fun_calc_t fun_calc = 0;
 static var _act(code& cd, int args)
 {
 	opr o = cd.oprstack.pop();
 
-	//PRINT("act:" << o << "(" << args << ")")
+	if (fun_calc)
+	{
+		if(!fun_calc(cd, o, args).empty())
+			return 0;
+	}
 
-		switch (o) {
-		case '+': {
-			if (args > 1) {
-				var& b = PHG_VALSTACK(1);
-				PRINTV(b.resid);
-				var& a = PHG_VALSTACK(2);
-				PRINTV(a.resid);
-				var ret = a + b;
-				PHG_VALPOP(2);
-				return ret;
-			}
-			else {
-				return cd.valstack.pop();
-			}
-		}
-		case '-': {
-			if (args > 1) {
-				var& b = PHG_VALSTACK(1);
-				var& a = PHG_VALSTACK(2);
-				var ret = a - b;
-				PHG_VALPOP(2);
-				return ret;
-			}
-			else {
-				return cd.valstack.pop();
-			}
-		}
-		case '*': {
-			if (args > 1) {
-				var& b = PHG_VALSTACK(1);
-				var& a = PHG_VALSTACK(2);
-				var ret = a * b;
-				PHG_VALPOP(2);
-				return ret;
-			}
-			else {
-				return cd.valstack.pop();
-			}
-		}
-		case '=': {
+	PRINT("calc:" << o << "(" << args << ")")
+
+	switch (o) {
+	case '+': {
+		if (args > 1) {
 			var& b = PHG_VALSTACK(1);
 			var& a = PHG_VALSTACK(2);
-			var ret = var(int(a == b));
+			var ret = a + b;
 			PHG_VALPOP(2);
 			return ret;
-			}
-		case '>': {
-			var& b = PHG_VALSTACK(1);
-			var& a = PHG_VALSTACK(2);
-			var ret = a > b;
-			PHG_VALPOP(2);
-			return ret;
-			}
-		case '<': {
-			var& b = PHG_VALSTACK(1);
-			var& a = PHG_VALSTACK(2);
-			var ret = a < b;
-			PHG_VALPOP(2);
-			return ret;
-			}
-		default: {}
 		}
+		else {
+			return cd.valstack.pop();
+		}
+	}
+	case '-': {
+		if (args > 1) {
+			var& b = PHG_VALSTACK(1);
+			var& a = PHG_VALSTACK(2);
+			var ret = a - b;
+			PHG_VALPOP(2);
+			return ret;
+		}
+		else {
+			return -cd.valstack.pop();
+		}
+	}
+	case '*': {
+		if (args > 1) {
+			var& b = PHG_VALSTACK(1);
+			var& a = PHG_VALSTACK(2);
+			var ret = a * b;
+			PHG_VALPOP(2);
+			return ret;
+		}
+		else {
+			return cd.valstack.pop();
+		}
+	}
+	case '=': {
+		var& b = PHG_VALSTACK(1);
+		var& a = PHG_VALSTACK(2);
+		var ret = var(int(a == b));
+		PHG_VALPOP(2);
+		return ret;
+		}
+	case '>': {
+		var& b = PHG_VALSTACK(1);
+		var& a = PHG_VALSTACK(2);
+		var ret = a > b;
+		PHG_VALPOP(2);
+		return ret;
+		}
+	case '<': {
+		var& b = PHG_VALSTACK(1);
+		var& a = PHG_VALSTACK(2);
+		var ret = a < b;
+		PHG_VALPOP(2);
+		return ret;
+		}
+	default: {}
+	}
 	return INVALIDVAR;
 }
